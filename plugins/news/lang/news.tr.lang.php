acc80cff37912a40ed2bc7606aaa794629f6da81
Seditio CMS <bilgi@ntka.org>
2022-07-07 16:21:29 +0300

    Create news.tr.lang.php

---
+++ plugins/news/lang/news.tr.lang.php
@@ -0,0 +1,21 @@
+<?php
+
+/* ====================
+Seditio - Website engine
+Copyright Neocrome & Seditio Team
+https://seditiocms.com
+
+[BEGIN_SED]
+File=plugins/news/lang/news.tr.lang.php
+Version=178
+Updated=2012-may-16
+Type=Plugin
+Author=Seditio Team
+Description=
+[END_SED]
+==================== */
+
+$L['cfg_category'] = array("Üst kategori kodu", "");
+$L['cfg_maxpages'] = array("Görüntülenen sayfa sayısı", "");
+
+?>
