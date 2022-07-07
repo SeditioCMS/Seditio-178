37c4ee9d03e238ce904d95455224b2a4d4b3539d
Seditio CMS <bilgi@ntka.org>
2022-07-07 16:41:44 +0300

    Create sedcaptcha.tr.lang.php

---
+++ plugins/sedcaptcha/lang/sedcaptcha.tr.lang.php
@@ -0,0 +1,21 @@
+<?PHP
+
+/* ====================
+Seditio - Website engine
+Copyright Neocrome & Seditio Team 
+http://www.seditiocms.com
+
+[BEGIN_SED]
+File=plugins/sedcaptcha/lang/sedcaptcha.tr.lang.php
+Version=178
+Updated=2022-jun-09
+Type=Plugin
+Author=Amro
+Description=PHP Captcha ile kayıt sürecini korumak için eklenti.
+[END_SED]
+==================== */
+
+$L['plu_verification_failed'] = "Doğrulama başarısız oldu!";
+$L['plu_scaptcha_noties'] = "okuyamıyorum?";
+
+?>
