53f4b6bbf9397a772bb060628387796b1549f6a7
Seditio CMS <bilgi@ntka.org>
2022-07-07 16:37:16 +0300

    Create recentitems.tr.lang.php

---
+++ plugins/recentitems/lang/recentitems.tr.lang.php
@@ -0,0 +1,22 @@
+<?php
+
+/* ====================
+Seditio - Website engine
+Copyright Neocrome & Seditio Team
+https://seditiocms.com
+
+[BEGIN_SED]
+File=plugins/recentitems/lang/recentitems.tr.lang.php
+Version=178
+Updated=2012-may-16
+Type=Plugin
+Author=Seditio Team
+Description=
+[END_SED]
+==================== */
+
+$L['cfg_maxpages'] = array("Görüntülenecek sayfa sayısı", "");
+$L['cfg_maxtopics'] = array("Görüntülenecek konu sayısı", "");
+$L['cfg_maxpolls'] = array("Görüntülenecek anket sayısı", "");
+
+?>
