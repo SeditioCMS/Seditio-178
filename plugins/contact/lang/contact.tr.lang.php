dfadf015088c4ed882e93c7710bd11eca111cdbb
Seditio CMS <bilgi@ntka.org>
2022-07-07 16:13:48 +0300

    Create contact.tr.lang.php

---
+++ plugins/contact/lang/contact.tr.lang.php
@@ -0,0 +1,39 @@
+<?PHP
+
+/* ====================
+Seditio - Website engine
+Copyright Neocrome & Seditio Team
+https://seditiocms.com
+[BEGIN_SED]
+File=plugins/lang/contact.tr.lang.php
+Version=178
+Updated=2022-jun-12
+Type=
+Author=Seditio Team
+Description=
+[END_SED]
+==================== */
+
+
+$L['plu_title'] = "İletişim";
+
+
+$L['plu_explain'] = "Bize bir e-posta göndermek için bu formu doldurun, mümkün olan en kısa sürede cevap vereceğiz !";
+$L['plu_recipients_title'] = "Alıcı";
+$L['plu_email_title'] = "Email";
+$L['plu_name_title'] = "İsim";
+$L['plu_phone_title'] = "Telefon";
+$L['plu_subject_title'] = "Konu";
+$L['plu_message_title'] = "Mesaj";
+$L['plu_required'] = "* = Gerekli";
+$L['plu_verify'] = "İnsan olduğunuzdan emin olmak için lütfen bu numarayı nokta olmadan tekrar girin : ";
+$L['plu_send'] = "Gönder";
+
+$L['plu_fieldempty'] = "En az bir alan boş.";
+$L['plu_wrongentry'] = "En az bir alanda hata var.";
+$L['plu_antispam'] = "Spambot koruma anahtarı yanlış, lütfen yeniden yazın !";
+$L['plu_notsent'] = "mesaj Gönderilemedi.";
+$L['plu_sent'] = "Mesaj başarıyla iletildi !";
+$L['plu_notice'] = "Bu mesaj şuradan gönderildi: ".$cfg['maintitle']." by : ";
+
+?>
