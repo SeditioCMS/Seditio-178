7b8d179229f85fd58731ec02548ad62643a1cb8e
Seditio CMS <bilgi@ntka.org>
2022-07-07 16:56:49 +0300

    Create whosonline.tr.lang.php

---
+++ plugins/whosonline/lang/whosonline.tr.lang.php
@@ -0,0 +1,26 @@
+<?PHP
+
+/* ====================
+Seditio - Website engine
+Copyright Neocrome & Seditio Team
+https://seditiocms.com
+[BEGIN_SED]
+File=plugins/whosonline/lang/whosonline.tr.lang.php
+Version=178
+Updated=2022-jun-12
+Type=
+Author=Seditio Team
+Description=
+[END_SED]
+==================== */
+
+$L['plu_title'] = "Kim çevrimiçi ?";
+$L['plu_mostonline'] = "Şimdiye kadar çevrimiçi olan çoğu kullanıcı ";
+$L['plu_therescurrently'] = "şu anda var ";
+$L['plu_visitors'] = " ziyaretçi(ler) ve ";
+$L['plu_members'] = " kayıtlı üye(ler) çevrimiçi.";
+$L['plu_lastseen1'] = "Son görülen";
+$L['plu_in'] = "içinde";
+$L['plu_visitor'] = "Ziyaretçi";
+
+?>
